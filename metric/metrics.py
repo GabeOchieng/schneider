7c127d589c91aa83dcda327721eae55897cbe31b
Peter Bull <pjbull@gmail.com>
2018-02-05 00:01:15 -0800

    Update consumption challenge data cleaning and metric

--- metric/metrics.py
+++ metric/metrics.py
@@ -1,41 +1,57 @@
 import numpy as np
 
 
-def _per_site_wrmse(actual, predicted):
-    # generate per-building weights according to
-    # wi = (3n –2i + 1) / (2n^2)
-    n_obs = actual.shape[0]
-    weights = np.arange(1, n_obs + 1, dtype=np.float64)
-    weights = (3 * n_obs - (2 * weights) + 1) / (2 * (n_obs ** 2))
+def _per_forecast_wrmse(actual, predicted, weights=None):
+    # limit weights to just the ones we need
+    weights = weights[:actual.shape[0]]
 
-    # NaNs should be weighted zero, but weight function untouched
-    weights[np.isnan(actual)] = 0
-    actual[np.isnan(actual)] = 0
+    # NaNs in the actual should be weighted zero
+    nan_mask = np.isnan(actual)
+    weights[nan_mask] = 0
+    actual[nan_mask] = 0
 
     # calculated weighted rmse
     total_error = np.sqrt((weights * ((predicted - actual) ** 2)).sum())
 
-    # normalized by actual consumption
-    return total_error / np.mean(actual)
+    # normalized by actual consumption (avoid division by zero for NaNs)
+    denom = np.mean(actual)
+    denom = denom if denom != 0.0 else 1e-10
+    return total_error / denom
 
 
 def weighted_rmse(actual, predicted):
-    """ col 0: timestamps
-        col 1: site id
-        col 2: consumption value
+    """ col 0: site id
+        col 1: timestamp
+        col 2: forecast id
+        col 3: consumption value
 
         Computes the weighted, normalized RMSE per site and then
         averages across sites for a final score.
     """
-    unique_sites = np.unique(actual[:, 1])
-    per_site_errors = np.zeros_like(unique_sites, dtype=np.float64)
+    # flatten and cast forecast ids
+    forecast_ids = actual[:, 2].ravel().astype(int)
 
-    for i, site in enumerate(unique_sites):
-        mask = actual[:, 1] == site
-        per_site_errors[i] = _per_site_wrmse(actual[mask, 2].ravel(),
-                                             predicted[mask, 2].ravel())
+    # flatten and cast actual + predictions
+    actual_float = actual[:, 3].ravel().astype(np.float64)
+    predicted_float = predicted[:, 3].ravel().astype(np.float64)
 
-    return np.mean(per_site_errors)
+    # get the unique forecasts
+    unique_forecasts = np.unique(forecast_ids)
+    per_forecast_errors = np.zeros_like(unique_forecasts, dtype=np.float64)
+
+    # pre-calc all of the possible weights so we don't need to do so for each site
+    # wi = (3n –2i + 1) / (2n^2)
+    n_obs = 200  # longest forecast is ~192 obs
+    weights = np.arange(1, n_obs + 1, dtype=np.float64)
+    weights = (3 * n_obs - (2 * weights) + 1) / (2 * (n_obs ** 2))
+
+    for i, forecast in enumerate(unique_forecasts):
+        mask = (forecast_ids == forecast)
+        per_forecast_errors[i] = _per_forecast_wrmse(actual_float[mask],
+                                                     predicted_float[mask],
+                                                     weights=weights)
+
+    return np.mean(per_forecast_errors)
 
 
 def test_weighted_rmse():
